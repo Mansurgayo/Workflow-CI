a2ffdb9a573cc4f0824dd86681d009411a9c44ea
Mansurgayo <mansur677776@gmail.com>
2025-11-22 06:06:16 +0700

    fianl

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -6,22 +6,29 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
 # Load dataset
-df = pd.read_csv("dataset/cleaned_dataset.csv")  # path relatif ke MLProject
+df = pd.read_csv("dataset/cleaned_dataset.csv")
 
+# Fitur & target
 X = df.drop("Outcome", axis=1)
 y = df["Outcome"]
 
+# Split data
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
+# Set MLflow experiment
 mlflow.set_experiment("Workflow_CI_Experiment")
 
 with mlflow.start_run():
+    # Train model
     model = RandomForestClassifier(n_estimators=100, random_state=42)
     model.fit(X_train, y_train)
+
+    # Predict & evaluate
     y_pred = model.predict(X_test)
     acc = accuracy_score(y_test, y_pred)
-    
+
+    # Log metric & model
     mlflow.log_metric("accuracy", acc)
     mlflow.sklearn.log_model(model, "model")
-    
+
     print(f"Model accuracy: {acc}")
