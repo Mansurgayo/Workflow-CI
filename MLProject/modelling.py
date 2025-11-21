e0df69693fb6afb82ca508956d9a9277a1e4df5c
Mansurgayo <mansur677776@gmail.com>
2025-11-22 05:57:10 +0700

    final

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,4 +1,3 @@
-import os
 import mlflow
 import mlflow.sklearn
 import pandas as pd
@@ -6,34 +5,23 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
-# Tentukan path dataset relatif terhadap script
-BASE_DIR = os.path.dirname(__file__)
-DATA_PATH = os.path.join(BASE_DIR, "dataset", "cleaned_dataset.csv")
-
 # Load dataset
-df = pd.read_csv(DATA_PATH)
+df = pd.read_csv("dataset/cleaned_dataset.csv")  # path relatif ke MLProject
 
-# Sesuaikan kolom target
-X = df.drop("Outcome", axis=1)  # Fitur
-y = df["Outcome"]               # Target
+X = df.drop("Outcome", axis=1)
+y = df["Outcome"]
 
-# Split data
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Set experiment MLflow
 mlflow.set_experiment("Workflow_CI_Experiment")
 
 with mlflow.start_run():
-    # Buat model
     model = RandomForestClassifier(n_estimators=100, random_state=42)
     model.fit(X_train, y_train)
-    
-    # Prediksi dan evaluasi
     y_pred = model.predict(X_test)
     acc = accuracy_score(y_test, y_pred)
     
-    # Log metric dan model ke MLflow
     mlflow.log_metric("accuracy", acc)
     mlflow.sklearn.log_model(model, "model")
-
+    
     print(f"Model accuracy: {acc}")
